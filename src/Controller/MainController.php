7b3d92aa873e77d0722e66c96face6c9ad9a1fcd
leerlandais <leerlandais@gmail.com>
2024-10-29 10:26:52 +0100

    added symfo to .env

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Section;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +13,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'public_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+
+        $authors = $entityManager->getRepository(User::class)->findAll();
+        $categs = $entityManager->getRepository(Section::class)->findAll();
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'authors' => $authors,
+            'cats' => $categs,
+          //  'arts' => $articleRepository->findAll(),
         ]);
     }
 }
