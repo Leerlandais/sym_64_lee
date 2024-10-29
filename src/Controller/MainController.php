c832f64aeee25430a6bbfdf97d4348ac29b58ddc
leerlandais <leerlandais@gmail.com>
2024-10-30 00:45:17 +0100

    docker is painfully slow, much easier to migrate the changes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -29,4 +29,61 @@ class MainController extends AbstractController
             'tags' => $tags,
         ]);
     }
+
+    #[Route('/author/{id}', name: 'public_author', methods: ['GET'])]
+    public function author(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $arts = $entityManager->getRepository(Article::class)->getArticlesByAuthorId($id);
+        $author = $entityManager->getRepository(User::class)->find($id);
+        return $this->render('main/author.html.twig', [
+            'author' => $author,
+            'arts' => $arts,
+        ]);
+    }
+
+    #[Route('/section/{slug}', name: 'public_section', methods: ['GET'])]
+    public function section(EntityManagerInterface $entityManager, string $slug): Response
+    {
+        $arts = $entityManager->getRepository(Article::class)->getArticlesBySectionSlug($slug);
+        foreach ($arts as $art) {
+            foreach ($art->getSections() as $sect) {
+                $sect->getSectionTitle();
+            }
+        }
+
+        return $this->render('main/section.html.twig', [
+            'section' => $sect,
+            'arts' => $arts,
+        ]);
+    }
+
+    #[Route('/tag/{slug}', name: 'public_tag', methods: ['GET'])]
+    public function tag(EntityManagerInterface $entityManager, string $slug): Response
+    {
+        $arts = $entityManager->getRepository(Article::class)->getArticlesByTagSlug($slug);
+
+        foreach ($arts as $art) {
+            foreach ($art->getTags() as $tag) {
+                $tag->getTagName();
+            }
+        }
+
+        return $this->render('main/tag.html.twig', [
+            'arts' => $arts,
+            'tag' => $tag,
+        ]);
+    }
+
+    #[Route('/art/{slug}', name: 'public_article', methods: ['GET'])]
+    public function article(EntityManagerInterface $entityManager, string $slug): Response
+    {
+        $article = $entityManager->getRepository(Article::class)->getArticlesByTitleSlug($slug);
+        $arts = $entityManager->getRepository(Article::class)->findAll();
+
+        return $this->render('main/article.html.twig', [
+            'article' => $article,
+            'arts' => $arts,
+        ]);
+    }
+
 }
